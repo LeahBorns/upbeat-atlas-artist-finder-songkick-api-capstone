3619e61bbddaa64a366cbbca2b3ff38593da867a
Leah Borns <leah.borns@gmail.com>
2017-09-21 10:57:28 -0700

    updating opening message and header height. Cleaned up code

--- script.js
+++ script.js
@@ -19,35 +19,6 @@ $(document).ready(function () {
         //use the value to call the getResults function defined below
         DataFromUser(userSearch);
 
-        //pop-up popup to explain travel options
-
-        /*$(function () {
-            // Get the popup
-            var modal = document.getElementById('myPopup');
-
-            // Get the button that opens the popup
-            var btn = document.getElementById("myBtn");
-
-            // Get the <span> element that closes the popup
-            var span = document.getElementsByClassName("close")[0];
-
-            // When the user clicks on the button, open the popup
-            btn.onclick = function () {
-                modal.style.display = "block";
-            }
-
-            // When the user clicks on <span> (x), close the popup
-            span.onclick = function () {
-                modal.style.display = "none";
-            }
-
-            // When the user clicks anywhere outside of the popup, close it
-            window.onclick = function (event) {
-                if (event.target == modal) {
-                    modal.style.display = "none";
-                }
-            }
-        });*/
 
         // nav sticks once scrolling
         $(function () {
